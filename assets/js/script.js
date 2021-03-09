87c2d2faf995094d118f258b0d9c8b96707b9d6b
Denis <dekor55@mail.ru>
2021-03-09 21:41:58 +0500

    Заказать звонок, меню

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
+
     $('.slider__container').bxSlider({
         pagerCustom: '.slider__pager',
         controls: false
     });
+
     $('.footer__arrow').on('click', function () {
         $('html, body').animate({ scrollTop: 0 }, 300);
     });
+
+    $('.header__callback-button, .header__feedback-button').on('click', function () {
+        $('.callback, .callback-bg').fadeIn();
+        return false;
+    });
+
+    $('.callback-bg, .callback-close').on('click', function () {
+        $('.callback, .callback-bg').fadeOut();
+    });
+
+    $('.header__menu-level-1').on('mouseenter', function () {
+        $(this).find('.header__menu-list').stop().slideDown(200);
+    });
+
+    $('.header__menu-level-1').on('mouseleave', function () {
+        $(this).find('.header__menu-list').stop().slideUp(200);
+    });
+
 });
